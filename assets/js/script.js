f5b8add7ba7620141ecf52736684fa41bf58e47e
Pablo <percara.pablo@gmail.com>
2023-01-18 21:56:34 -0300

    errores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 
-
 let boton = document.querySelector('#boton')
 let boton_volver = document.querySelector('#boton_volver')
 let boton_descargar = document.querySelector('#boton_descarga')
@@ -18,18 +17,31 @@ boton_descargar.addEventListener('click', descargar)
 
 function iniciar(){
 
-  let titulo = document.querySelector('#titulo').value
-  let etiquetas = document.querySelector('#etiquetas').value
-  let labels = etiquetas.split(',');
-  let valores = document.querySelector('#valores').value
-  let data = valores.split(',');
-  let tipo = document.querySelector('#tipos').value 
+  let titulo = document.querySelector('#titulo')
+  if((titulo.value).trim()==''){
+    titulo.setAttribute('placeholder', 'Debe completar este campo!')
+  }
   
+  let etiquetas = document.querySelector('#etiquetas')
+  if((etiquetas.value).trim()==''){
+    etiquetas.setAttribute('placeholder', 'Debe completar este campo!')
+  }
+  
+  let valores = document.querySelector('#valores')
+  if((valores.value).trim()==''){
+    valores.setAttribute('placeholder', 'Debe completar este campo!')
+  }
+ 
+  let tipo = document.querySelector('#tipos').value 
+
+  
+  titulo = document.querySelector('#titulo').value
+  etiquetas = document.querySelector('#etiquetas').value
+  valores = document.querySelector('#valores').value
+  let labels = etiquetas.split(',');
+  let data = valores.split(',');
 
   let grafico = document.getElementById('grafico').getContext('2d');
-
-  
-
 
   new Chart(grafico, {
     type: tipo,
@@ -51,7 +63,6 @@ function iniciar(){
   });
   
 }
-
 
 function ocultar(){ 
   document.getElementById('contenedor-grafico').classList.toggle('oculta')
